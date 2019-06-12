dcb2cdc9ba76f12135935a25d30a2a012e0158b7
BrokenFlows <cianfdowd@gmail.com>
2019-06-12 15:25:25 +0100

    New fish functions - git status and shortcut to project directory

---
+++ config/fish/functions/gs.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function gs --description 'Get git status, but with command like ls'
+	git status
+end
---
+++ config/fish/functions/mep.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function mep --description 'Go to project practical files'
+	cd ~/ME/Practical/eeen40240/
+end
